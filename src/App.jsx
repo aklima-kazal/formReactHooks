3d13451ef2c75a8e5b7756addc0e31527d5074a6
aklima-kazal <aklimakajal11@gmail.com>
2025-09-04 19:54:11 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -34,19 +34,19 @@ const App = () => {
       return;
     }
     const newBoard = [...board];
-    newBoard[index] = isXTurn ? "X" : "O";
+    newBoard[index] = isXTurn ? "X" : "0";
     setBoard(newBoard);
     setIsXTurn(!isXTurn);
   }
   function getGameStatus() {
     const winner = getWinner(board);
     if (winner) {
-      return `Winner: ${winner}`;
+      return `Winner : ${winner}`;
     }
     if (board.every((square) => square !== null)) {
       return "Draw";
     }
-    return "Next player: " + (isXTurn ? "X" : "O");
+    return "Next player : " + (isXTurn ? "X" : "0");
   }
   function resetGame() {
     setBoard(Array(9).fill(null));
@@ -54,29 +54,29 @@ const App = () => {
   }
   return (
     <>
-      <div className=" mx-auto flex items-center justify-center min-h-screen max-w-4xl bg-linear-to-bl from-violet-300 to-red-200 ">
+      <div className=" mx-auto flex items-center justify-center min-h-[100vh] w-[100vw] bg-linear-to-bl from-violet-300 to-red-200 ">
         <div>
-          <h1 className="text-4xl text-cyan-800 font-bold text-center text-shadow-lg/30 ...">
+          <h1 className="text-6xl text-cyan-800 font-bold text-center text-shadow-lg/30 ...">
             Tik Tac Toe
           </h1>
           <div
-            className={`text-2xl text-center mt-2 text-shadow-lg/30 ... ${
+            className={`text-3xl text-center mt-4 text-shadow-lg/30 ... ${
               getWinner(board)
-                ? "text-green-800 animate-bounce"
-                : "text-red-500"
+                ? "text-red-900 animate-bounce"
+                : "text-violet-900 animate-pulse"
             }`}
           >
             {getGameStatus()}
           </div>
-          <div className="grid grid-cols-3 gap-4 p-4 shadow-xl/30 ... bg-linear-to-bl from-gray-800 to-slate-500 overflow-hidden rounded-xl mt-10 mb-6 w-[400px]">
+          <div className="grid grid-cols-3 gap-2 p-4 shadow-xl/30 ... bg-linear-to-bl from-gray-800 to-slate-500 overflow-hidden rounded-xl mt-6 mb-6 ">
             {board.map((squares, index) => (
               <button
                 key={index}
-                className={` bg-slate-900 rounded-2xl text-4xl h-32 mx-auto flex items-center justify-center font-bold w-[90%] text-shadow-lg/30 ... shadow-xl/30 ... ${
+                className={`hover:bg-slate-950 bg-slate-900 rounded-2xl text-5xl h-32 mx-auto flex items-center justify-center font-bold w-[100%] text-shadow-lg/30 ... shadow-xl/30 ... ${
                   squares === "X"
-                    ? "text-pink-800"
-                    : squares === "O"
-                    ? "text-violet-800"
+                    ? "text-pink-500 font-bold text-6xl"
+                    : squares === "0"
+                    ? "text-violet-500 text-6xl font-bold"
                     : ""
                 }`}
                 onClick={() => handleSquereClick(index)}
